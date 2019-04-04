5751a5fb5213ef5abb8e3dc63e8f1884a3efaa61
Aldo_A <aldoalmeida@aldos-macbook-pro.local>
2019-04-03 20:31:37 -0400

    New Screen

---
+++ GuessRandom/Mainscreen.swift
@@ -0,0 +1,30 @@
+//
+//  Mainscreen.swift
+//  GuessRandom
+//
+//  Created by Aldo Almeida on 4/3/19.
+//  Copyright © 2019 Life X. All rights reserved.
+//
+
+import UIKit
+
+class Mainscreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
