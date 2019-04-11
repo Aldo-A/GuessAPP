571a259448e141a7096342c4d654d0b31b507ba0
Aldo-A <44143466+Aldo-A@users.noreply.github.com>
2019-04-10 20:38:29 -0400

    fix merge errors and added game functionality

---
+++ GuessAPP 2/GuessRandom/easyGame.swift
@@ -0,0 +1,81 @@
+//
+//  easyGame.swift
+//  GuessRandom
+//
+//  Created by Aldo Almeida on 4/9/19.
+//  Copyright © 2019 Life X. All rights reserved.
+//
+
+import UIKit
+
+class easyGame: UIViewController {
+
+    @IBOutlet weak var min: UILabel!
+    @IBOutlet weak var max: UILabel!
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var attemptsLeft: UILabel!
+    
+    var minimum:Int=0
+    var maxmimum:Int=10
+    var specialNumber:Int=0
+    var numOfGuesses=5
+    
+    @IBAction func getGuess(_ sender: UIButton) {
+        var guess=Int(input.text!)
+        
+        if(guess==specialNumber)
+        {
+            attemptsLeft.text="Winner"
+            
+        }
+        else
+        {
+            if(numOfGuesses>0)
+            {
+                numOfGuesses=numOfGuesses-1
+                attemptsLeft.text=String(numOfGuesses)
+            }
+                
+            else
+            {
+                print("lost")
+            }
+        }
+        
+        
+        
+    }
+    
+    @IBAction func backToStart(_ sender: UIButton) {
+        performSegue(withIdentifier: "backToHome", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        calculateNumbers()
+    }
+    
+    private func calculateNumbers()
+    {
+        minimum=Int.random(in: 0..<90)
+        maxmimum=minimum+10
+        specialNumber=Int.random(in: minimum..<maxmimum)
+        min.text=String(minimum)
+        max.text=String(maxmimum)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
